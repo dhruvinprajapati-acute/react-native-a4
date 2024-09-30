ed1bbc177bdf4f8b71687bd5ddcc332b14e34228
Dhruvin Prajapati <dhruvin.prajapati@acuteinformatics.in>
2024-09-30 10:40:29 +0530

    adding data on redux

---
+++ src/redux/reducer.ts
@@ -0,0 +1,18 @@
+import { ADD_TO_CART } from "./constants";
+
+const initialState = [];
+
+export const reducer=(state=initialState, action)=>{
+    switch (action.type) {
+        case ADD_TO_CART:
+            return [
+                ...state, action.data
+            ]
+            break;
+    
+        default:
+            return state
+            break;
+    }
+
+}
